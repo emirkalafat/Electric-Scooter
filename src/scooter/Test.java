a605e3b92b3d4d869089d83d827137eaba3dc081
emirkalafat <bilgisayaremir@hotmail.com>
2021-01-14 14:49:57 +0300

    aynı anda iki scooter kullanılamaz kuralı düzeltmesi

--- src/scooter/Test.java
+++ src/scooter/Test.java
@@ -31,12 +31,16 @@ public class Test {
 
 
         surucu3.bakiyeYukle(20);
+
+
         System.out.println("***************************");
         istasyon1.araclariListele();
         istasyon2.araclariListele();
         istasyon3.araclariListele();
 
         System.out.println("***************************");
+
+
         surucu3.araciTeslimAl(istasyon1);
 
         System.out.println("***************************");
